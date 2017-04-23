be3c658f5be769298f4bd7a4cce69d46fb73c4a8
Johnny Chen <mcjohnnychen@gmail.com>
2017-04-23 12:28:29 -0700

    changes styles to allow js to set up board, also added an approach to the puzzle

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,44 @@
 'use strict';
 
 
-console.log('test');
+
+// 0) initialize tiles and place into position
+// 1) create 3x3 grid with nested for loop
+// 2) attach event listeners
+// 3) allow only tiles that are adjacent to empty node be clickable
+// 4) detect empty space
+// 5) when tile is clicked, move clicked tile to empty node
+
+
+
+
+// padding (10) + width (68) of a tile = 2nd position
+// aka positions are in multiples of 78
+
+var delta_x = 78;
+var delta_y = 78;
+
+
+function positionTiles(){
+
+}
+
+function mapGrid(){
+
+}
+
+function checkAdjacent(){
+
+}
+
+function moveTile(e){
+
+}
+
+
+function init(){
+  var tiles = document.getElementsByClassName('tile');
+
+
+
+}
