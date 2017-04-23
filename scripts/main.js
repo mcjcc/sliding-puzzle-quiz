0c8989ba4ee7f2112ee5eca678f3f028651a1abb
Johnny Chen <mcjohnnychen@gmail.com>
2017-04-23 13:56:26 -0700

    added function to position tiles

--- scripts/main.js
+++ scripts/main.js
@@ -15,30 +15,68 @@
 // padding (10) + width (68) of a tile = 2nd position
 // aka positions are in multiples of 78
 
-var delta_x = 78;
-var delta_y = 78;
+var tileWidth  =  68;
+var tileHeight =  68;
+var padding = 10;
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  //do work
+  function positionTiles(tiles){
+    console.log('positionTiles starting');
+
+    for (var i=0; i < tiles.length; i++){
+      console.log('for loop');
+      switch(tiles[i].innerText) {
+        case "1":
+            break;
+        case "2":
+            tiles[i].style.left = tileWidth + padding + "px";
+            break;
+        case "3":
+            tiles[i].style.left = 2 * tileWidth + padding + "px";
+            break;
+        case "4":
+            tiles[i].style.top = tileHeight + padding + "px";
+            break;
+        case "5":
+            tiles[i].style.left = tileWidth + padding + "px";
+            tiles[i].style.top = tileHeight + padding + "px";
+            break;
+        case "6":
+            tiles[i].style.left = 2 * tileWidth + padding + "px";
+            tiles[i].style.top = tileHeight + padding + "px";
+            break;
+        case "7":
+            tiles[i].style.top = 2 * tileHeight + padding + "px";
+            break;
+        case "8":
+            tiles[i].style.top = 2 * tileHeight + padding + "px";
+            tiles[i].style.left = tileWidth + padding + "px";
+            break;
+      }
+    }
+  }
+
+  function mapGrid(){
+
+  }
+
+  function checkAdjacent(){
+
+  }
+
+  function moveTile(e){
+
+  }
 
 
-function positionTiles(){
+  function init(){
+    var tiles = document.getElementsByClassName('tile');
+    console.log(tiles);
+    positionTiles(tiles);
 
-}
+    console.log('init starting');
+  }
+  init();
 
-function mapGrid(){
-
-}
-
-function checkAdjacent(){
-
-}
-
-function moveTile(e){
-
-}
-
-
-function init(){
-  var tiles = document.getElementsByClassName('tile');
-
-
-
-}
+});
